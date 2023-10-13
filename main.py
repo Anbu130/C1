48f9a6bc5aab8b37bb83a9cf7ecb5d79a40d3c73
Anbu130 <143778711+Anbu130@users.noreply.github.com>
2023-10-13 12:37:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+stdne=input("")
+stdtrno=int(input(""))
+stdc=input("")
+mkpyt=int(input(""))
+mkja=int(input(""))
+mkds=int(input(""))
+mkhtml=int(input(""))
+mkcss=int(input(""))
+total=(mkpyt+mkja+mkds+mkhtml+mkcss)
+print(total)
+avg=(total/500)*100
+print(avearage)
+marks=100
+if mk>=90:
+print("A+grade")
+print("very goood")
+elif mk A=80:
+print("a grade")
+print("good")
+elif mk>=70:
+print("B+ grade")
+print("average")
+elif marks>=60:
+print("Bgrade")
+print("below average")
+else:
+print("UgradE")
+print("Ra")
